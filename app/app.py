51cc12493f8a47826d1eb85c27000fe56a71a809
mariabeaalyssa <beaalyssa1@gmail.com>
2019-11-11 14:03:44 +0800

    set up

---
+++ app/app.py
@@ -0,0 +1,8 @@
+from app import *
+
+from flask import render_template, request, redirect, url_for, flash
+
+
+@server.route('/', methods=["GET","POST"])
+def landingpage():
+	return render_template('landingpage.html', title="Inpack")
